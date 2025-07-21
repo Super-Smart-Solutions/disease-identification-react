d957cc16531f98184a89ebe27448f38a7f2a89fb
Mahmoud Ayman <mahmoud.ayman07@gmail.com>
2025-07-21 15:22:54 +0300

    updated image review modal and added trans

--- src/Components/pages/admin/images/ImageReviewer.jsx
+++ src/Components/pages/admin/images/ImageReviewer.jsx
@@ -8,6 +8,8 @@ import { FiChevronLeft, FiChevronRight, FiTrash2 } from 'react-icons/fi';
 import { useQuery } from '@tanstack/react-query';
 import { fetchDiseasesByPlant, fetchPlants } from '../../../../api/plantAPI';
 import ConfirmationModal from '../../../ConfirmationModal';
+import ImageViewer from './ImageViewer.jsx'
+
 
 const ImageReviewer = ({ t, onClose }) => {
   const [selectedPlant, setSelectedPlant] = useState(null);
@@ -100,34 +102,25 @@ const ImageReviewer = ({ t, onClose }) => {
         )}
       </Formik>
 
-      <div className="relative w-full h-96 bg-gray-100 rounded-lg flex items-center justify-center">
-        {isLoading && <p>{t('loading_key')}</p>}
-        {!isLoading && currentImage && (
-          <img
-            src={currentImage.url}
-            alt={currentImage.name}
-            className="max-h-full max-w-full object-contain"
-          />
-        )}
-        {!isLoading && !currentImage && <p>{t('no_images_found_key')}</p>}
-      </div>
+      <ImageViewer
+        t={t}
+        currentImage={currentImage}
+        isLoading={isLoading}
+        handleBack={handleBack}
+        handleNext={handleNext}
+      />
 
-      <div className="flex justify-between items-center pt-4">
-        <Button onClick={handleBack} disabled={isLoading || !currentImage}>
-          <FiChevronLeft className="mr-2" />
-          {t('back_key')}
-        </Button>
+      <div className="flex justify-center items-center pt-4">
         <Button
           variant="danger"
+          size="large"
+          width="auto"
           onClick={() => setImageToDelete(currentImage)}
           disabled={isLoading || !currentImage}
+          className="flex items-center space-x-2"
         >
-          <FiTrash2 className="mr-2" />
-          {t('delete_key')}
-        </Button>
-        <Button onClick={handleNext} disabled={isLoading || !currentImage}>
-          {t('next_key')}
-          <FiChevronRight className="ml-2" />
+          <FiTrash2  />
+          <span>{t('delete_key')}</span>
         </Button>
       </div>
 
